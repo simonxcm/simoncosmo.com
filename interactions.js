2e46624ca8161e20551ca07087ed5109604e7b50
simoncm <mollardsimon11@gmail.com>
2022-01-26 23:19:47 +0100

    add all the webm videos and JS play and pause functions

--- interactions.js
+++ interactions.js
@@ -16,8 +16,6 @@ let topValue = header.offsetTop;
 
 function myFunction() {
 
-    console.log(topValue);
-
     if (window.pageYOffset > topValue) {
         header.classList.add("sticky");
         contentBelow.classList.add("sticky-padding");
@@ -27,4 +25,77 @@ function myFunction() {
         header.classList.remove("sticky");
         contentBelow.classList.remove("sticky-padding");
     }
+}
+
+
+// *----------------------------------------------------* //
+// *---------------  PLAY VIDEO ON HOVER  --------------* //
+// *----------------------------------------------------* //
+
+
+let videoOne = document.getElementById("video-one");
+let videoTwo = document.getElementById("video-two");
+let videoThree = document.getElementById("video-three");
+let videoFour = document.getElementById("video-four");
+let videoFive = document.getElementById("video-five"); 
+let videoSix = document.getElementById("video-six"); 
+
+function playVid1()
+{ 
+    videoOne.play();  
+} 
+
+function pauseVid1()
+{ 
+    videoOne.pause();
+}
+
+function playVid2()
+{ 
+    videoTwo.play();  
+} 
+
+function pauseVid2()
+{ 
+    videoTwo.pause();
+}
+
+function playVid3()
+{ 
+    videoThree.play();  
+} 
+
+function pauseVid3()
+{ 
+    videoThree.pause();
+}
+
+function playVid4()
+{ 
+    videoFour.play();  
+} 
+
+function pauseVid4()
+{ 
+    videoFour.pause();
+}
+
+function playVid5()
+{ 
+    videoFive.play();  
+} 
+
+function pauseVid5()
+{ 
+    videoFive.pause();
+}
+
+function playVid6()
+{ 
+    videoSix.play();  
+} 
+
+function pauseVid6()
+{ 
+    videoSix.pause();
 }
